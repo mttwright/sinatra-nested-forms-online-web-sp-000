a00b3ff84abf196eb554dfe7bf1f1363b3cc1d97
Matt Wright <mttwright@gmail.com>
2021-01-17 14:27:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,31 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    
+    get '/' do
+      "Welcome to the Nested Forms Lab!"
+    end
+    
+    get '/new' do
+      erb :'/pirates/new'
+    end
+    
+    post '/pirates' do
+      
+      
+      @pirate = Pirate.new(params[:pirate])
+      
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+      
+      @ships = Ship.all
+      
+      erb :'/pirates/show'
+      
+    end
+    
+    
 
   end
 end
